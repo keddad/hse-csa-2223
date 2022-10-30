2fb013f6d5fc81c43a02bef247dd386b520894a2
keddad <keddad@yandex.ru>
2022-10-30 18:52:38 +0300

    ihw2: 6 points

--- ihw2/test_asm.py
+++ ihw2/test_asm.py
@@ -4,7 +4,7 @@ import itertools
 from subprocess import run, PIPE, STDOUT
 from pathlib import Path
 
-binaries = ["ref", "ref_commented"]
+binaries = ["ref", "ref_commented", "ref_regopt"]
 
 tests = [("abc\ncde\n", "ab\nde"),
          ("aaa\naaa", ""),
